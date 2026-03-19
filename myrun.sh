8fbd3ed28093bc83e5a342af099c86d8c335804d
Seiya Kumada <seiya.kumada@gmail.com>
2026-03-19 18:20:42 +0900

    最初のコミット

--- myrun.sh
+++ myrun.sh
@@ -3,19 +3,20 @@
 #uv run python -m src.main
 
 #_/_/_/ 画像1枚をGeminiに投げる。
-#ROOT_DIR_PATH="/home/kumada/data/depth_image_aligner/"
-#IMAGE_SUB_DIR_PATH="images"
-#FILE_NAME="C_Sample3"
+ROOT_DIR_PATH="/home/kumada/data/depth_image_aligner/"
+OUTPUT_DIR_PATH="/home/kumada/data/gemini_workspace/"
+IMAGE_SUB_DIR_PATH="dragon_2.5"
+FILE_NAME="dragon_2.5"
 #OUTPUT_SUB_DIR_PATH="gemini_outputs"
-#MAX_TOKENS=8192
-#TOP_P=1.0
-#TEMPERATURE=0.0
-#uv run python -m src.invoke_gemini \
-#    --input_path "${ROOT_DIR_PATH}/${IMAGE_SUB_DIR_PATH}/${FILE_NAME}.jpg" \
-#    --output_path "${ROOT_DIR_PATH}/${OUTPUT_SUB_DIR_PATH}/${FILE_NAME}.txt" \
-#    --max_tokens ${MAX_TOKENS} \
-#    --top_p ${TOP_P} \
-#    --temperature ${TEMPERATURE}
+MAX_TOKENS=8192
+TOP_P=1.0
+TEMPERATURE=0.0
+uv run python -m src.invoke_gemini \
+    --input_path "${ROOT_DIR_PATH}/${IMAGE_SUB_DIR_PATH}/${FILE_NAME}.jpg" \
+    --output_path "${OUTPUT_DIR_PATH}/${FILE_NAME}.txt" \
+    --max_tokens ${MAX_TOKENS} \
+    --top_p ${TOP_P} \
+    --temperature ${TEMPERATURE}
 
 #_/_/_/ 動画一本をGeminiに投げる。
 #ROOT_DIR_PATH="/home/kumada/data/gemini_workspace/"
@@ -50,19 +51,19 @@
 #    --temperature ${TEMPERATURE}
 
 #_/_/_/ 動画1本と画像1枚をGeminiに投げる。
-ROOT_DIR_PATH="/home/kumada/data/gemini_workspace/"
-MOVIE_SUB_DIR_PATH="movies"
-IMAGE_SUB_DIR_PATH="images"
-MOVIE_FILE_NAME="C_Sample3"
-IMAGE_FILE_NAME="zumen"
-OUTPUT_SUB_DIR_PATH="outputs"
-MAX_TOKENS=8192
-TOP_P=1.0
-TEMPERATURE=0.0
-uv run python -m src.invoke_gemini_with_image_and_movie \
-    --image_path "${ROOT_DIR_PATH}/${IMAGE_SUB_DIR_PATH}/${IMAGE_FILE_NAME}.png" \
-    --movie_path "${ROOT_DIR_PATH}/${MOVIE_SUB_DIR_PATH}/${MOVIE_FILE_NAME}.mp4" \
-    --output_path "${ROOT_DIR_PATH}/${OUTPUT_SUB_DIR_PATH}/${MOVIE_FILE_NAME}_${IMAGE_FILE_NAME}.txt" \
-    --max_tokens ${MAX_TOKENS} \
-    --top_p ${TOP_P} \
-    --temperature ${TEMPERATURE}
+#ROOT_DIR_PATH="/home/kumada/data/gemini_workspace/"
+#MOVIE_SUB_DIR_PATH="movies"
+#IMAGE_SUB_DIR_PATH="images"
+#MOVIE_FILE_NAME="C_Sample3"
+#IMAGE_FILE_NAME="zumen"
+#OUTPUT_SUB_DIR_PATH="outputs"
+#MAX_TOKENS=8192
+#TOP_P=1.0
+#TEMPERATURE=0.0
+#uv run python -m src.invoke_gemini_with_image_and_movie \
+#    --image_path "${ROOT_DIR_PATH}/${IMAGE_SUB_DIR_PATH}/${IMAGE_FILE_NAME}.png" \
+#    --movie_path "${ROOT_DIR_PATH}/${MOVIE_SUB_DIR_PATH}/${MOVIE_FILE_NAME}.mp4" \
+#    --output_path "${ROOT_DIR_PATH}/${OUTPUT_SUB_DIR_PATH}/${MOVIE_FILE_NAME}_${IMAGE_FILE_NAME}.txt" \
+#    --max_tokens ${MAX_TOKENS} \
+#    --top_p ${TOP_P} \
+#    --temperature ${TEMPERATURE}
--- src/invoke_gemini.py
+++ src/invoke_gemini.py
@@ -12,7 +12,7 @@ import os
 from google import genai
 from google.genai import types
 
-import src.input_prompts.input_prompt_for_3d_recognition_2026_02_18_with_field as ip
+import src.input_prompts.input_prompt_for_dragon_2_5 as ip
 import src.utils as utils
 
 
